625683d3380489ee9ecf0a0cd3b86d63fb40a92f
Oleg Cherednik <oleg.cherednik@gmail.com>
2021-05-16 08:55:16 +0300

    README.md

--- src/main/java/ru/olegcherednik/utils/reflection/Consumer.java
+++ src/main/java/ru/olegcherednik/utils/reflection/Consumer.java
@@ -1,13 +1,19 @@
 package ru.olegcherednik.utils.reflection;
 
 /**
+ * @param <T> the type of the input to the operation
  * @author Oleg Cherednik
  * @since 11.05.2021
  */
 public interface Consumer<T> {
 
-    Consumer<?> NULL = (Consumer<Object>)obj -> { };
+    Consumer<?> NULL = (Consumer<Object>)t -> { };
 
+    /**
+     * Performs this operation on the given argument.
+     *
+     * @param t the input argument
+     */
     void accept(T t) throws Exception;
 
 }
--- src/main/java/ru/olegcherednik/utils/reflection/Function.java
+++ src/main/java/ru/olegcherednik/utils/reflection/Function.java
@@ -1,11 +1,19 @@
 package ru.olegcherednik.utils.reflection;
 
 /**
+ * @param <T> the type of the input to the function
+ * @param <R> the type of the result of the function
  * @author Oleg Cherednik
  * @since 11.05.2021
  */
 public interface Function<T, R> {
 
+    /**
+     * Applies this function to the given argument.
+     *
+     * @param t the function argument
+     * @return the function result
+     */
     R apply(T t) throws Exception;
 
 }
